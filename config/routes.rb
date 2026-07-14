cb48580011091dc23bd91b7b46dfad864781fd61
erik petrowski <erik.richard.petrowski@gmail.com>
2026-07-14 15:08:38 +0200

    Created functionality to create a new loop

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :loops, only: [ :index, :destroy ]
+
+  resources :loops, only: [:index, :new, :create, :edit, :update, :destroy]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   get "analyse", to: "analyse#index", as: :analyse_index
